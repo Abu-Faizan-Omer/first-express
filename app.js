742364a540f59ca23f3786bf2871fde055eb278f
Abu-Faizan-Omer <abufaizan1997@gmail.com>
2024-10-05 11:51:08 +0530

    with get and post

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const http=require("http")
 
 const express=require("express")
+const bodyparser=require("body-parser")
 const app=express()
-app.use((req,res,next)=>{
-    console.log("Inside middleware")
-    next()
+
+app.use(bodyparser.urlencoded({extended:false}))//use like a buffer we convert into readable //parse only form body not JSON,files etc
+
+app.use("/add-product",(req,res,next)=>{
+    //console.log("Welcome to about page")
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form')   
+})
+app.post("/product",(req,res,next)=>{
+    console.log(req.body)
+    res.redirect("/")
 })
 
-app.use((req,res,next)=>{
-    console.log("second middle ware")
-    res.send("<h1>hello from express js</h1>")
+app.use("/",(req,res,next)=>{
+    //console.log("home")
+    res.send("<h1>WElcome to Home page!</h1>")
 })
 const server=http.createServer(app)
-server.listen(3000)
+app.listen(3000)
